7e156d41b73d454c0d066221d6471e55f029f161
sergio <serginho111level@gmail.com>
2021-05-08 01:10:47 -0300

    versao 1 do codigo pronto

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 let totalslider = document.querySelectorAll('.slider-item').length
 document.querySelector('.slider-width').style.width = ` calc(100vw * ${totalslider})`
-document.querySelector('.slider-controles').style.height = `${document.querySelector('.slider').clientHeight}px`
 let currentSlider = 0
 
-function gonext(){
-    currentSlider++
-    if(currentSlider === totalslider ){
-        currentSlider = totalslider - totalslider
-    }
-    attMargin()
-    console.log('next'+ currentSlider)
+for(let i = 0 ; i < totalslider ; i++){
+        document.querySelector('.sliders-pointers').innerHTML += `<div class="pointer" id="bannerSlider${i}" onclick="gonext(${i})" ></div>`
 }
-function goprev(){
-    currentSlider--
-    if(currentSlider === -1 ) {
-        currentSlider = totalslider - 1
+console.log( 1 == 1 ? 'sim' : 'nao')
+// funçao que recebe o valor do click para percorrer o slide de fomra manual
+function gonext(n){
+    currentSlider = n
+    for(let i = 0 ; i < totalslider ; i++){
+        document.getElementById(`bannerSlider${i}`).style.backgroundColor = 'transparent'
     }
-    attMargin()
-    console.log('prev'+ currentSlider)
+    if(currentSlider === totalslider) {
+        currentSlider = 0
+        document.getElementById(`bannerSlider${currentSlider}`).style.backgroundColor = 'rgb(4, 101, 192)'
+    }
+    document.getElementById(`bannerSlider${currentSlider}`).style.backgroundColor = 'rgb(4, 101 , 192)'
+    document.querySelector('.slider-width').style.marginLeft = `-${currentSlider * 100}vw`
 }
+// funcao que auomatiza o slider
 function attMargin(){
+    currentSlider++
+    for(let i = 0 ; i < totalslider ; i++){
+        document.getElementById(`bannerSlider${i}`).style.backgroundColor = 'transparent'
+    }
+    if(currentSlider === totalslider) {
+        currentSlider = 0
+        document.getElementById(`bannerSlider${currentSlider}`).style.backgroundColor = 'rgb(4, 101, 192)'
+    }
+    document.getElementById(`bannerSlider${currentSlider}`).style.backgroundColor = 'rgb(4, 101 , 192)'
     let sliderwidth = document.querySelector('.slider-item').clientWidth
     let newMargin = (currentSlider * sliderwidth )
     document.querySelector('.slider-width').style.marginLeft = `-${newMargin}px`
 } 
-setInterval(gonext , 5000)
+setInterval( attMargin , 5000)
 
 /*
 outra logica simples de se usar para atualizar a margem dos sliders é usando 'vw' ao inves de 'px'
