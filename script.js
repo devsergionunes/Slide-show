5dc528aaa229d5fc38831f56616f53843a7f9c21
sergio <serginho111level@gmail.com>
2021-05-08 01:21:18 -0300

    versao final com codigo 100% reutilizavel

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ let currentSlider = 0
 for(let i = 0 ; i < totalslider ; i++){
         document.querySelector('.sliders-pointers').innerHTML += `<div class="pointer" id="bannerSlider${i}" onclick="gonext(${i})" ></div>`
 }
-console.log( 1 == 1 ? 'sim' : 'nao')
-// funçao que recebe o valor do click para percorrer o slide de fomra manual
+// funçao que recebe o valor do click para percorrer o slide de fomra manual ou o valor se auto incrementa por conta da funçao setInterval
 function gonext(n){
-    currentSlider = n
+    n !== undefined ? currentSlider = n : currentSlider++
     for(let i = 0 ; i < totalslider ; i++){
         document.getElementById(`bannerSlider${i}`).style.backgroundColor = 'transparent'
     }
@@ -18,27 +17,14 @@ function gonext(n){
     }
     document.getElementById(`bannerSlider${currentSlider}`).style.backgroundColor = 'rgb(4, 101 , 192)'
     document.querySelector('.slider-width').style.marginLeft = `-${currentSlider * 100}vw`
-}
-// funcao que auomatiza o slider
-function attMargin(){
-    currentSlider++
-    for(let i = 0 ; i < totalslider ; i++){
-        document.getElementById(`bannerSlider${i}`).style.backgroundColor = 'transparent'
-    }
-    if(currentSlider === totalslider) {
-        currentSlider = 0
-        document.getElementById(`bannerSlider${currentSlider}`).style.backgroundColor = 'rgb(4, 101, 192)'
-    }
-    document.getElementById(`bannerSlider${currentSlider}`).style.backgroundColor = 'rgb(4, 101 , 192)'
-    let sliderwidth = document.querySelector('.slider-item').clientWidth
-    let newMargin = (currentSlider * sliderwidth )
-    document.querySelector('.slider-width').style.marginLeft = `-${newMargin}px`
 } 
-setInterval( attMargin , 5000)
+setInterval( gonext , 5000)
 
 /*
 outra logica simples de se usar para atualizar a margem dos sliders é usando 'vw' ao inves de 'px'
 function attMargin(){
-    document.querySelector('.slider-width').style.marginLeft = `-${currentSlider * 100}vw`
+    let sliderwidth = document.querySelector('.slider-item').clientWidth
+    let newMargin = (currentSlider * sliderwidth )
+    document.querySelector('.slider-width').style.marginLeft = `-${newMargin}px`
 }
 */
